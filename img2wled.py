4da11cb8c786a1cb46066abe47ad855442215be8
David Moreno <dmoreno@coralbits.com>
2023-01-01 18:58:16 +0100

    Can use command line parameters

--- img2wled.py
+++ img2wled.py
@@ -1,20 +1,22 @@
 #!/bin/python3
 
+from dataclasses import dataclass
 import json
-import sys
+import argparse
+import requests
 
 from PIL import Image, ImageSequence
 
 
-ROWS = 16
-COLS = 16
-IP = "192.168.1.33"
 MAX_PIXELS = 8*16
+args = None
 
 
 def gen_str_from_img(frame):
     frame = frame.convert("RGB")
-    frame = frame.resize((ROWS, COLS), resample=Image.NEAREST)
+    rows = int(args.rows)
+    cols = int(args.cols)
+    frame = frame.resize((rows, cols), resample=Image.NEAREST)
     lastpx = None
     pixels = frame.load()
     commands = [1]
@@ -23,8 +25,8 @@ def gen_str_from_img(frame):
     pxn = 0
     runl = 0
     lpxn = 0
-    for c in range(COLS):
-        for r in range(ROWS):
+    for c in range(cols):
+        for r in range(rows):
             lpxn += 1
             pxn += 1
             px = pixels[r, c]
@@ -55,16 +57,34 @@ def gen_str(image):
             yield from gen_str_from_img(frame)
     else:
         yield from gen_str_from_img(img)
+def setup():
+    global args
+    parser = argparse.ArgumentParser(
+                    prog = 'img2wled',
+                    description = 'Convert any image to curl commands',
+                    epilog = 'To be used to print images a WLED matrixes.')    
 
+    parser.add_argument('filename', nargs=1)          
+    parser.add_argument('-c', '--cols', help="Column count", default=16)
+    parser.add_argument('-r', '--rows', help="Row count", default=16)      
+    parser.add_argument('--ip', help="IP to send to.", default="wled.local")      
+    parser.add_argument('--curl', help="Print curl commands, instead of directly executing the request", action="store_true", default=False)      
+    args = parser.parse_args()
 
 def main():
-    if sys.argv[1:]:
-        img = sys.argv[1]
-    else:
-        img = IMG
+    setup()
+
+    img = args.filename[0]
+    if not img:
+        print("Required image file name")
+        return
+    # img = sys.argv[1]
     for segment in gen_str(img):
-        print(
-            f"curl -X POST 'http://{IP}/json/state' -H 'Content-Type: application/json' -d '{json.dumps(segment)}'")
+        if args.curl:
+            print(
+                f"curl -X POST 'http://{args.ip}/json/state' -H 'Content-Type: application/json' -d '{json.dumps(segment)}'")
+        else:
+            requests.post(f"http://{args.ip}/json/śtate", data=json.dumps(segment), headers={"Content-Type": "application/json"})
 
 
 if __name__ == '__main__':
